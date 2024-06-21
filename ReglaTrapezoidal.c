efc7ed03239b8e90c983bc61a48f49451c734016
JaviS <sarango.2013.30@gmail.com>
2024-06-21 00:18:58 -0500

    ReglaTrapezoidal

--- ReglaTrapezoidal.c
+++ ReglaTrapezoidal.c
@@ -4,81 +4,80 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Definición de la función a integrar
+// Definición de la función que se va a integrar
+// f(x) = (x^3) + (x^2) + (x + 1)
 double f(double x) {
     return ((x * x * x) + (x * x) + (x + 1));
 }
 
 // Regla trapezoidal para un intervalo dado
-double trapezoidal_rule(double local_a, double local_b, int local_n, double h) {
-    double integral;
+double regla_Trapezoidal(double local_a, double local_b, int local_n, double h) {
+    double resultado;
     double x;
     int i;
 
-    integral = (f(local_a) + f(local_b)) / 2.0;
+    resultado = (f(local_a) + f(local_b)) / 2.0;
     x = local_a;
     for (i = 1; i <= local_n-1; i++) {
         x += h;
-        integral += f(x);
+        resultado += f(x);
     }
-    integral *= h;
+    resultado *= h;
 
-    return integral;
+    return resultado;
 }
 
 int main(int argc, char** argv) {
-    int my_rank, comm_sz;
-    long int n, local_n;
+    int rank, size, n, local_n;
     double a, b, h, local_a, local_b;
-    double local_integral, total_integral;
-    double start_time, end_time;
+    double areaCalculada, areaMostrar;
+    double tiempoInicio, tiempoFin;
 
-    // Inicializar MPI
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-    // Solo el proceso 0 obtiene los valores de entrada
-     if (my_rank == 0) {
+     if (rank == 0) {
         printf("Ingrese a, b, y n\n");
-        scanf("%lf %lf %ld", &a, &b, &n);
+        scanf("%lf %lf %d", &a, &b, &n);
     }
 
-
-    // Transmitir los valores de a, b, y n a todos los procesos
     MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Calcular h y local_n
     h = (b-a)/n;
-    local_n = n/comm_sz;
+    local_n = n/size;
 
-    // Calcular los intervalos locales
-    local_a = a + my_rank*local_n*h;
+    // Calculo de los intervalos de manera local, en donde:
+    /*
+    local_a es el extremo inferior de la regla trapezoidal y
+    local_b es el extremo superior
+    */
+    local_a = a + rank*local_n*h;
     local_b = local_a + local_n*h;
 
-    // Sincronización y inicio del tiempo
+    // Sincronización y inicio de toma de tiempo
     MPI_Barrier(MPI_COMM_WORLD);
-    start_time = MPI_Wtime();
+    tiempoInicio = MPI_Wtime();
 
-    // Calcular la integral local
-    local_integral = trapezoidal_rule(local_a, local_b, local_n, h);
+    // Calcular el area con la formula de la regla trapezoidal
+    areaCalculada = regla_Trapezoidal(local_a, local_b, local_n, h);
 
-    // Reducir todas las integrales locales a la integral total
-    MPI_Reduce(&local_integral, &total_integral, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    // El reduce obtiene el area de todos los procesos
+    MPI_Reduce(&areaCalculada, &areaMostrar, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    // Sincronización y fin del tiempo
-    end_time = MPI_Wtime();
+    // Capturar el tiempo de ejecución de finalizacion
+    tiempoFin = MPI_Wtime();
 
-    // Proceso 0 imprime el resultado y el tiempo de ejecución
-    if (my_rank == 0) {
-        printf("Con n = %ld trapezoides\n", n);
-        printf("El area bajo la curva de %f a %f = %.15e\n", a, b, total_integral);
-        printf("El tiempo de ejecución fue %f segundos\n", end_time - start_time);
+    // Mostrar resultado por parte del proceso 0
+    if (rank == 0) {
+        printf("Con n = %d trapezoides\n", n);
+        printf("El area bajo la curva de %f a %f = %.15e\n", a, b, areaMostrar);
+        printf("El tiempo de ejecución fue %f segundos\n", tiempoFin - tiempoInicio);
     }
 
-    // Finalizar MPI
     MPI_Finalize();
 
     return 0;
