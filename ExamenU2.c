a5264eee2b519363e1c480d9ddbdc58bb188487b
JaviS <sarango.2013.30@gmail.com>
2024-06-25 11:58:30 -0500

    ExamenU2

--- ExamenU2.c
+++ ExamenU2.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 int main(int argc, char **argv)
 {
     int N, i;
     int *V, sum = 0;
+    double tiempoInicio, tiempoFin;
+    printf("Ejecución en Serie\n\n");
+
     printf("\nLongitud del vector: ");
     scanf("%d", &N); // Multiplo del numero de procesos
     V = (int *)malloc(sizeof(int) * N);
+    tiempoInicio = clock() / (double)CLOCKS_PER_SEC;
     for (i = 0; i < N; i++)
         V[i] = rand() % 100 - 50; // inicializacion del vector
     // Procesamiento del vector
@@ -15,9 +21,10 @@ int main(int argc, char **argv)
         sum = sum + V[i];
     for (i = 0; i < N; i++)
         V[i] = V[i] * sum;
+    tiempoFin = clock() / (double)CLOCKS_PER_SEC;
     // Imprimir unos resultados
     printf("\n sum = %d\n", sum);
+    printf("\n Tiempo de ejecucion: %.6f segundos\n", tiempoFin - tiempoInicio);
     printf("\n V[0] = %d, V[N/2] = %d, V[N-1] = %d\n\n\n", V[0], V[N / 2], V[N - 1]);
     return 0;
 }
-
