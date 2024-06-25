a77fb4d062032121586184a80600bb3f4b40adf0
JaviS <sarango.2013.30@gmail.com>
2024-06-25 12:06:24 -0500

    ExamenU2

--- ExamenU2.c
+++ ExamenU2.c
@@ -12,6 +12,20 @@ int main(int argc, char **argv)
 
     printf("\nLongitud del vector: ");
     scanf("%d", &N); // Multiplo del numero de procesos
+
+    ejecucionSerie(N);
+    printf("\nEjecución en MPI\n\n");
+    ejecucionMPI(); 
+
+    return 0;
+}
+
+
+void ejecucionSerie(int N)
+{
+    int i;
+    int *V, sum = 0;
+    double tiempoInicio, tiempoFin;
     V = (int *)malloc(sizeof(int) * N);
     tiempoInicio = clock() / (double)CLOCKS_PER_SEC;
     for (i = 0; i < N; i++)
@@ -26,5 +40,52 @@ int main(int argc, char **argv)
     printf("\n sum = %d\n", sum);
     printf("\n Tiempo de ejecucion: %.6f segundos\n", tiempoFin - tiempoInicio);
     printf("\n V[0] = %d, V[N/2] = %d, V[N-1] = %d\n\n\n", V[0], V[N / 2], V[N - 1]);
-    return 0;
+
+}
+
+void ejecucionMPI()
+{
+    int N, i, size, rank;
+    int *V, sum = 0;
+    double tiempoInicio, tiempoFin;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0)
+    {
+        printf("\nLongitud del vector: ");
+        scanf("%d", &N); // Multiplo del numero de procesos
+        V = (int *)malloc(sizeof(int) * N);
+        tiempoInicio = MPI_Wtime();
+        for (i = 0; i < N; i++)
+            V[i] = rand() % 100 - 50; // inicializacion del vector
+    }
+
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    V = (int *)malloc(sizeof(int) * N);
+
+    int chunk = N / size;
+    int start = rank * chunk;
+    int end = (rank + 1) * chunk;
+    if (rank == size - 1)
+        end = N;
+
+    for (i = start; i < end; i++)
+        sum = sum + V[i];
+
+    MPI_Reduce(&sum, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+        for (i = 0; i < N; i++)
+            V[i] = V[i] * sum;
+        tiempoFin = MPI_Wtime();
+        // Imprimir unos resultados
+        printf("\n sum = %d\n", sum);
+        printf("\n Tiempo de ejecucion: %.6f segundos\n", tiempoFin - tiempoInicio);
+        printf("\n V[0] = %d, V[N/2] = %d, V[N-1] = %d\n\n\n", V[0], V[N / 2], V[N - 1]);
+    }
+
+    MPI_Finalize();
 }
