4a84b70d230fad6046be56969e50b8a1372e5ccc
JaviS <sarango.2013.30@gmail.com>
2024-08-01 04:52:06 -0500

    EvaluacionFinal

--- EvaluacionFinal.c
+++ EvaluacionFinal.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define ROOT 0
+#define RAIZ 0
 
 // Función para enviar valores a los nodos hijos
 void enviar_a_hijos(int valor, int num_procesos) {
     int num_hijos = 2;
     int hijos[num_hijos];
     for (int i = 0; i < num_hijos; i++) {
-        hijos[i] = ROOT * num_hijos + i + 1;
+        hijos[i] = RAIZ * num_hijos + i + 1;
     }
 
     for (int i = 0; i < num_hijos && hijos[i] < num_procesos; i++) {
@@ -21,10 +21,11 @@ void enviar_a_hijos(int valor, int num_procesos) {
 int recibir_y_sumar(int num_procesos) {
     int num_hijos = 2;
     int suma = 0;
-    for (int i = 0; i < num_hijos && ROOT * num_hijos + i + 1 < num_procesos; i++) {
+    for (int i = 0; i < num_hijos && RAIZ * num_hijos + i + 1 < num_procesos; i++) {
         int valor_recibido;
         MPI_Recv(&valor_recibido, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         suma += valor_recibido;
+        printf("Nodo %d recibió valor %d de su hijo\n", RAIZ, valor_recibido);
     }
     return suma;
 }
@@ -40,7 +41,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &num_procesos);
 
     if (argc != 2) {
-        if (rango == ROOT) {
+        if (rango == RAIZ) {
             fprintf(stderr, "Uso: %s <valor_inicial>\n", argv[0]);
         }
         MPI_Finalize();
@@ -49,32 +50,33 @@ int main(int argc, char *argv[]) {
 
     valor_inicial = atoi(argv[1]);
 
-    if (rango == ROOT) {
+    if (rango == RAIZ) {
         tiempo_inicio = MPI_Wtime();
     }
 
     // Lógica de envío y recepción de mensajes
-    if (rango == ROOT) {
+    if (rango == RAIZ) {
         enviar_a_hijos(valor_inicial, num_procesos);
         int resultado = recibir_y_sumar(num_procesos) + valor_inicial;
-        if (num_procesos > 1) {
-            printf("Resultado final en el nodo 0: %d\n", resultado);
-        }
+        printf("Resultado final en el nodo 0: %d\n", resultado);
     } else {
         int valor_recibido;
-        MPI_Recv(&valor_recibido, 1, MPI_INT, ROOT, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&valor_recibido, 1, MPI_INT, RAIZ, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         int valor_suma = valor_recibido;
+        printf("Nodo %d recibió valor %d de su padre\n", rango, valor_recibido);
+
         if (rango * 2 + 1 < num_procesos) {
             enviar_a_hijos(valor_suma, num_procesos);
             int suma_hijos = recibir_y_sumar(num_procesos);
             valor_suma += suma_hijos;
         }
 
-        MPI_Send(&valor_suma, 1, MPI_INT, ROOT, 0, MPI_COMM_WORLD);
+        MPI_Send(&valor_suma, 1, MPI_INT, RAIZ, 0, MPI_COMM_WORLD);
+        printf("Nodo %d envió suma parcial %d al nodo padre\n", rango, valor_suma);
     }
 
-    if (rango == ROOT) {
+    if (rango == RAIZ) {
         tiempo_fin = MPI_Wtime();
         printf("Tiempo de ejecución: %f segundos\n", tiempo_fin - tiempo_inicio);
     }
